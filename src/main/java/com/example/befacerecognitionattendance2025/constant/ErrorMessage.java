716041f088a24bddf2208b5a3e5d71e43134e832
Khuong Xuan Toan <khuongxuantoan@gmail.com>
2025-09-20 02:30:03 +0700

    Setup/cloudinary (#4)
    * add mapstruct and cloudiary to pom.xml
    
    * add file upload feature using Cloudinary

--- src/main/java/com/example/befacerecognitionattendance2025/constant/ErrorMessage.java
+++ src/main/java/com/example/befacerecognitionattendance2025/constant/ErrorMessage.java
@@ -2,6 +2,8 @@ package com.example.befacerecognitionattendance2025.constant;
 
 public class ErrorMessage {
 
+    public static String INVALID_IMAGE_FILE = "Invalid Image File";
+
     public static class Validation {
         public static final String NOT_BLANK = "Không thể trống";
     }
